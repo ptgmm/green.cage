dfb03c123b989f80ce89981a7291ad63eb9a6bba
ptgmm <ptgmm@proton.me>
2024-11-02 19:55:48 +0100

    Check for commits

--- main.c
+++ main.c
@@ -4,16 +4,39 @@
 
 
 int main(){
-    
-    char domain[50];
-    char command[50];
-    FILE *hostlist = fopen("/etc/hosts", "a");
-    
-    printf("Enter a domain: ");
-    scanf("%24s", domain);
+    const char *command = "git log --after=\"today 00:00\" --before=\"tomorrow 00:00\"";
 
-    fprintf(hostlist, "127.0.0.1    %s\n", domain);
-    fclose(hostlist);
+
+    FILE *fp;
+    fp = popen(command, "r");
+    if (fp == NULL){
+        perror("Error");
+    }
+    
+    char result[256];
+    int command_check;
+
+    while (fgets(result, sizeof(result), fp) != NULL){
+        command_check = 1;
+        break;
+    }
+
+
+    if (command_check){
+        printf("good\n");
+    } else{
+        printf("not good\n");
+    }
+
+
+    
+//    FILE *hostlist = fopen("/etc/hosts", "a");
+    
+//      printf("Enter a domain: ");
+//      scanf("%24s", domain);
+
+//    fprintf(hostlist, "127.0.0.1    %s\n", domain);
+//    fclose(hostlist);
 
 
 
