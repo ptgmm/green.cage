caf9742a7b3360c4bf7e32ece6fb86025ca7fa69
ptgmm <ptgmm@proton.me>
2024-11-03 21:51:06 +0100

    loop until commit

--- main.c
+++ main.c
@@ -3,10 +3,11 @@
 #include <stdlib.h>
 
 
+
 int main(){
     const char *command = "git log --since=\"yesterday\"";
-
-
+    int i;
+    char domain[50];
     FILE *fp;
     fp = popen(command, "r");
     if (fp == NULL){
@@ -16,33 +17,30 @@ int main(){
     char result[256];
     int command_check;
 
-    while (fgets(result, sizeof(result), fp) != NULL){
-        command_check = 1;
-        break;
+    while(1){
+        if (command_check == 1){
+            printf("good\n");
+            break;
+        }
+/*        } else{
+            FILE *hostlist = fopen("/etc/hosts", "a");
+    
+            printf("Enter a domain: ");
+            scanf("%24s", domain);
+
+            fprintf(hostlist, "127.0.0.1    %s\n", domain);
+            fclose(hostlist); 
+        }*/
+
     }
 
 
-        // Close the command stream
     if (pclose(fp) == -1) {
         perror("pclose failed");
         return 1;
     }
 
-    if (command_check){
-        printf("good\n");
-    } else{
-        printf("not good\n");
-    }
-
-
-    
-//    FILE *hostlist = fopen("/etc/hosts", "a");
-    
-//      printf("Enter a domain: ");
-//      scanf("%24s", domain);
-
-//    fprintf(hostlist, "127.0.0.1    %s\n", domain);
-//    fclose(hostlist);
+         
 
 
 
