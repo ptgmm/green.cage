ca1892151e8d9f7924bb96e044546ec03c0a8edb
ptgmm <ptgmm@proton.me>
2024-11-05 23:06:46 +0100

    Change while loop to for lopp; I must figure how to make background process

--- main.c
+++ main.c
@@ -24,15 +24,18 @@ int main(){
         time(&current_time);
         elapsed_time = difftime(current_time, start_time);
         if (elapsed_time >= 10.0){
-            while (fgets(result, sizeof(result), fp) != NULL){
+            for (char result[256]; fgets(result, sizeof(result), fp) != NULL; ) {
+                FILE *hostlist = fopen("/etc/hosts", "a");
+                fprintf(hostlist, "\n127.0.0.1    example.com\n");
+                break;
+            }
+            for (char result[256]; fgets(result, sizeof(result), fp) == NULL; ) {
                 command_check = 1;
                 FILE *hostlist = fopen("/etc/hosts", "w");
                 fprintf(hostlist, "//");
+                break;
             }
-            while (fgets(result, sizeof(result), fp) == NULL){
-                FILE *hostlist = fopen("/etc/hosts", "a");
-                fprintf(hostlist, "127.0.0.1    example.com\n");
-            }
+
             start_time = current_time;
             break;
         }
@@ -40,3 +43,5 @@ int main(){
 
     return 0;
 }
+
+
